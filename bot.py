72b1245a4ba81bfb62a3354e4ab54a7132ea6468
i <grat.zj@gmail.com>
2022-08-03 18:04:12 +0300

    webhook

--- bot.py
+++ bot.py
@@ -77,28 +77,18 @@ async def main():
 
 
 
-    # start
-    try:
-        # scheduler.start()
-        start_webhook(
-            dispatcher=dp,
-            webhook_path=WEBHOOK_PATH,
-            skip_updates=True,
-            on_startup=on_startup,
-            on_shutdown=on_shutdown,
-            host=WEBAPP_HOST,
-            port=WEBAPP_PORT,
-        )
-    finally:
-        await bot.delete_webhook()
-        await dp.storage.close()
-        await dp.storage.wait_closed()
-        await bot.session.close()   
+  
 
 
 if __name__ == '__main__':
-    try:
-        asyncio.run(main())
-        
-    except (KeyboardInterrupt, SystemExit):
-        logger.error("Bot stopped!")
+    asyncio.run(main())
+    # start
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        skip_updates=True,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
